fa5af1cb416c1617e8e59b11add3ccdc79a78699
ethelrani22 <ethelrani6@gmail.com>
2026-07-10 08:31:53 +0530

    config: switch baseUrl to Android emulator (10.0.2.2)

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'https://dental-management-api-hjeh.onrender.com';
+  static const String baseUrl = 'http://10.0.2.2:8000';
 
   // API Endpoints
   static const String authRegister = '/api/auth/register';
